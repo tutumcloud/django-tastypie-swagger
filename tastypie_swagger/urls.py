af5c8cf3fc2b9c2ff081dd94c031da6f0625eddd
Fernando Mayo <fermayo@gmail.com>
2014-01-20 18:13:32 +0100

    redirect throbber.gif to avoid 404

--- tastypie_swagger/urls.py
+++ tastypie_swagger/urls.py
@@ -2,6 +2,8 @@ try:
 	from django.conf.urls import patterns, include, url
 except ImportError:
 	from django.conf.urls.defaults import patterns, include, url
+from django.conf import settings
+from django.views.generic.base import RedirectView
 
 from .views import SwaggerView, ResourcesView, SchemaView
 
@@ -10,4 +12,5 @@ urlpatterns = patterns('',
     url(r'^resources$', ResourcesView.as_view(), name='resources'),
     url(r'^resources/(?P<resource>\S+)$', SchemaView.as_view()),
     url(r'^resources$', SchemaView.as_view(), name='schema'),
+    url(r'^images/throbber\.gif', RedirectView.as_view(url='%stastypie_swagger/images/throbber.gif' % settings.STATIC_URL)),
 )
